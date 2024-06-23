fdbd4598ebf25375e9eb3d0095553c2922f38eac
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-06-23 15:59:53 +0530

    feat: close #2, add neotree toggle

--- init.lua
+++ init.lua
@@ -28,6 +28,16 @@ local plugins = {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' }
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  },
 }
 
 
@@ -44,6 +54,7 @@ require("lazy").setup(plugins, opts)
 local telescope = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', telescope.find_files, {})
 vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
+vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>')
 -- Set up theme
 require("tokyonight").setup()
 vim.cmd.colorscheme "tokyonight-night"
