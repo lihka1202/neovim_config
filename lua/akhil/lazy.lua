9d8b4f4d58789b93397f7635aa13fae19e8eea95
lihka1202 <mnakhilwork@gmail.com>
2024-12-27 17:59:56 +0530

    chore: add change notification on lazy.lua

--- lua/akhil/lazy.lua
+++ lua/akhil/lazy.lua
@@ -11,4 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("akhil.plugins")
+require("lazy").setup("akhil.plugins", {
+  cheker = {
+    enabled = true,
+    notify = false,
+  },
+})
